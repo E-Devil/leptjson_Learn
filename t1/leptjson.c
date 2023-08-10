54a788d5a4a3723d81eb95c7ef0436c5e3306ae3
E-Devil <1336218207@qq.com>
2023-08-10 11:48:45 +0800

    v1

--- t1/leptjson.c
+++ t1/leptjson.c
@@ -312,8 +312,10 @@ static int lept_parse_array(lept_context*c, lept_value* v){
     for(;;){
         lept_value e;
         lept_init(&e);
-        if( (ret = lept_parse_value(c, &e) != LEPT_PARSE_OK) )
+        if( (ret = lept_parse_value(c, &e) != LEPT_PARSE_OK) ){
+            lept_context_pop(c, size);
             return ret;
+        }
         memcpy(lept_context_push(c, sizeof(lept_value)), &e, sizeof(lept_value));
         size++;
         lept_parse_whitespace(c);
@@ -327,8 +329,10 @@ static int lept_parse_array(lept_context*c, lept_value* v){
             memcpy(v->u.a.e = (lept_value*)malloc(size), lept_context_pop(c, size), size);
             return LEPT_PARSE_OK;
         }
-        else
+        else{
+            lept_context_pop(c, size);
             return LEPT_PARSE_MISS_COMMA_OR_SQUARE_BRACKET;
+        }
     }
 }
 
